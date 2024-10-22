3ab5f4382995403b213eea744a6fb40c62f403a2
Aarik Guy <aarik15dg@gmail.com>
2024-10-22 10:41:45 -0600

    transferring from my repo to group repo. NFAState is done

---
+++ fa/nfa/NFA.java
@@ -0,0 +1,115 @@
+package fa.nfa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import fa.State;
+
+public class NFA implements NFAInterface{
+    private Set<NFAState> states;
+    private Set<Character> sigma;
+    private Set<NFAState> finalStates;
+
+    public NFA() {
+        states = new HashSet<>();
+        sigma = new HashSet<>();
+        finalStates = new HashSet<>();
+    }
+
+    @Override
+    public Set<NFAState> getToState(NFAState from, char onSymb) {
+        return from.getTransition(onSymb);
+    }
+
+    @Override
+    public Set<NFAState> eClosure(NFAState s) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int maxCopies(String s) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean isDFA() {
+        for (NFAState state : states) {
+            for (Character symbol : sigma) {
+                Set<NFAState> transitions = state.getTransition(symbol);
+                if (transitions.size() > 1 || state.getEpsilonTransitions().size() > 0) {
+                    return false; 
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addState(String name) {
+        if (getState(name) != null) {
+            return false; 
+        }
+        states.add(new NFAState(name)); 
+        return true;
+    }
+
+    @Override
+    public boolean setFinal(String name) {
+        NFAState state = getState(name);
+        if (state == null) {
+            return false; 
+        }
+        finalStates.add(state); 
+        return true;
+    }
+
+    @Override
+    public boolean setStart(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setStart'");
+    }
+
+    @Override
+    public void addSigma(char symbol) {
+        sigma.add(symbol);
+    }
+
+    @Override
+    public boolean accepts(String s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+    }
+
+    @Override
+    public Set<Character> getSigma() {
+        return new HashSet<>(sigma);
+    }
+
+    @Override
+    public NFAState getState(String name) {
+        for (NFAState state : states) {
+            if (state.getName().equals(name)) {
+                return state; 
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isFinal(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isFinal'");
+    }
+
+    @Override
+    public boolean isStart(String name) {
+        NFAState state = (NFAState) getState(name);
+        return state != null && state.isStart;
+    }
+    
+}
---
+++ fa/nfa/NFAState.java
@@ -0,0 +1,104 @@
+package fa.nfa;
+
+import fa.State;
+import java.util.*;
+
+/**
+ * This class represents a state in a Non-deterministic Finite Automaton (NFA).
+ * It extends the abstract class State and manages transitions to other states
+ * based on input symbols.
+ */
+public class NFAState extends State {
+
+    protected Map<Character, Set<NFAState>> transitions;
+    protected Set<NFAState> epsilonTransitions;
+    protected  boolean isStart;
+    protected String name;
+
+    /**
+     * Constructs an NFAState with the given name.
+     * 
+     * @param name The name of the state.
+     */
+    public NFAState(String name) {
+        this.name = name;
+        this.isStart = false; 
+        this.epsilonTransitions = new HashSet<>();
+        this.transitions = new HashMap<>();
+    }
+
+    /**
+     * Adds a transition from this state to another state based on the input symbol.
+     * If a transition on the given symbol already exists, the destination state is added
+     * to the set of possible states.
+     * 
+     * @param symbol The input symbol for the transition.
+     * @param toState The state to transition to on the given symbol.
+     */
+    public void setTransition(Character symbol, NFAState toState) {
+        Set<NFAState> toStates = transitions.get(symbol);
+        if (toStates == null) {
+            toStates = new HashSet<>();
+            transitions.put(symbol, toStates);
+        }
+        toStates.add(toState);
+    }
+
+    /**
+     * Retrieves the set of states that can be reached from this state on a given symbol.
+     * 
+     * @param symbol The input symbol.
+     * @return A set of states that can be transitioned to on the input symbol.
+     *         If no transition exists for the symbol, returns an empty set.
+     */
+    public Set<NFAState> getTransition(Character symbol) {
+        if (transitions.get(symbol) != null) {
+            return transitions.get(symbol);
+        }
+        return null;
+    }
+
+    /**
+     * Marks this state as the start state of the NFA.
+     * 
+     * @param isStart True to mark this state as the start state, false otherwise.
+     */
+    public void setStartState(boolean isStart) {
+        this.isStart = isStart;
+    }
+
+    /**
+     * Checks if this state is the start state of the NFA.
+     * 
+     * @return True if this is the start state, false otherwise.
+     */
+    public boolean isStartState() {
+        return isStart;
+    }
+
+    /**
+     * Gets the name of the state.
+     * 
+     * @return name
+     */
+    public String getName() {
+        return name; 
+    }
+
+    /**
+     * Gets epsilon transition.
+     * @return epsilonTransitions
+     */
+    public Set<NFAState> getEpsilonTransitions() {
+        return epsilonTransitions; 
+    }
+
+    /**
+     * Adds an epsilon transition.
+     * 
+     * @param toState
+     */
+    public void addEpsilonTransition(NFAState toState) {
+        epsilonTransitions.add(toState);
+    }
+}
